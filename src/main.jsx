4d88b558f15e6b5724b5efa66d29b22695133487
Isidro Navarro Oporto <99091821+IsiNavarro@users.noreply.github.com>
2023-09-22 23:12:21 +0200

    Install react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 
 import Home from './components/Home';
+import Catalogue from './components/Catalogue';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  { path: '/catalogue', element: <Catalogue /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
