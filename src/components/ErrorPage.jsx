012d31fea5c0bee21601a58b32ce3d9a2415be7f
Isidro Navarro Oporto <99091821+IsiNavarro@users.noreply.github.com>
2023-09-25 14:02:58 +0200

    Update ErrorLanding page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -3,9 +3,14 @@ import { Link } from 'react-router-dom';
 const ErrorPage = () => {
   return (
     <>
-      <div>
-        Oops! This page does not exist.{" let's go back to the "}
-        <Link to={'/'}>MAIN PAGE</Link>.
+      <div className="px-4 h-screen flex flex-col gap-2 items-center justify-center">
+        <h1 className="font-bold text-4xl text-slate-900">Oops!</h1>
+        <h2 className="font-bold text-2xl text-slate-700 text-center">
+          This page does not exist.
+        </h2>
+        <button className="font-bold text-lg bg-slate-950 text-white px-10 py-1 mt-2">
+          <Link to={'/'}>Home page</Link>
+        </button>
       </div>
     </>
   );
